3ffbdeae46bf0343905d72dc9b46f35c362424f1
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2019-10-28 10:48:55 +0100

    Add error control

--- src/xmlcontects/XmlContects.java
+++ src/xmlcontects/XmlContects.java
@@ -4,6 +4,9 @@
  * and open the template in the editor.
  */
 package xmlcontects;
+import java.io.File;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
 import org.dom4j.Document;
 import org.dom4j.DocumentHelper;
 import org.dom4j.Element;
@@ -17,8 +20,31 @@ public class XmlContects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
+        
+        //Constrisce filepath
+         String fileName = "xmlTest.xml";
+		String path = "./";
+        
+        //fa partire la grafica
         Gui gui = new Gui();
         gui.doAll();
+        
+         //controllo se esiste il file
+        File f = new File(fileName+path);
+         if(!f.exists()){
+            System.out.println("Non c'e'");
+            final JPanel panel = new JPanel();
+            JOptionPane.showMessageDialog(panel, "The contats file doesen't exist, one will be create", "Error", JOptionPane.ERROR_MESSAGE);
+         }else{
+             final JPanel panel = new JPanel();
+             JOptionPane.showMessageDialog(panel, "The file exist, you can go!", "All okey", JOptionPane.INFORMATION_MESSAGE);
+         }
+               
+               
+                
+                
+   
+        
         Document document = DocumentHelper.createDocument();
         Element root = document.addElement("root");
 
