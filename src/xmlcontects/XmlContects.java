6d9df8ef58b4e1f9400b838cb92afbb59ab2daa7
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2019-10-28 10:25:22 +0100

    First commit

---
+++ src/xmlcontects/XmlContects.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package xmlcontects;
+
+/**
+ *
+ * @author 72873486
+ */
+public class XmlContects {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+    
+}
