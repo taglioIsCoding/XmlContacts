694f71427e3a480ba01d9ba2e25240ce5004f242
Michele Tagliani <45893037+taglioIsCoding@users.noreply.github.com>
2019-10-28 10:37:10 +0100

    Add first gui

--- src/xmlcontects/XmlContects.java
+++ src/xmlcontects/XmlContects.java
@@ -4,7 +4,9 @@
  * and open the template in the editor.
  */
 package xmlcontects;
-
+import org.dom4j.Document;
+import org.dom4j.DocumentHelper;
+import org.dom4j.Element;
 /**
  *
  * @author 72873486
@@ -15,7 +17,23 @@ public class XmlContects {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        
+        Gui gui = new Gui();
+        gui.doAll();
+        Document document = DocumentHelper.createDocument();
+        Element root = document.addElement("root");
+
+        Element author1 = root.addElement("author")
+            .addAttribute("name", "James")
+            .addAttribute("location", "UK")
+            .addText("James Strachan");
+
+        Element author2 = root.addElement("author")
+            .addAttribute("name", "Bob")
+            .addAttribute("location", "US")
+            .addText("Bob McWhirter");
+
+   
+    
     }
     
 }
